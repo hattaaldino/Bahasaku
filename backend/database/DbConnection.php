b83f556b3d6d03c17999e354f4ebe9bea50a1ac5
Hatta Aldino <44059549+hattaaldino@users.noreply.github.com>
2020-08-24 03:10:28 +0700

    Fix DB Connection

--- backend/database/DbConnection.php
+++ backend/database/DbConnection.php
@@ -12,7 +12,7 @@ class DbConnection{
 		$this->con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 		if(mysqli_connect_errno()){
-			echo "Failed to connect to database : " . mysqli_connect_errno();
+			echo "Failed to connect to database : " . mysqli_connect_error();
 		}
 
 		return $this->con;
